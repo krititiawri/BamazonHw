8d1df254a15f9aba0ce00e8cc933c35672da7f29
krititiawri <kriit.tiwari28@gmail.com>
2017-09-19 02:30:00 -0500

    removing node_modules

--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -3,7 +3,9 @@ USE bamazon;
 CREATE TABLE products(
 	item_id INT NOT NULL AUTO_INCREMENT,
 	product_name VARCHAR(50),
+	product_sales DECIMAL(10,2),
 	department_name VARCHAR(50),
 	price DECIMAL(10,2),
+	stock_quantity INT(11),
 	PRIMARY KEY (item_id)
 );
